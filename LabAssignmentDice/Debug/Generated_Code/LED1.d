44a6832374bae85a73d518af0a63cac6460d5c84
Sienna Price <siennarose.price@my.jcu.edu.au>
2017-09-09 17:40:09 +1000

    asdfjkl;

---
+++ LabAssignmentDice/Debug/Generated_Code/LED1.d
@@ -0,0 +1,28 @@
+Generated_Code/LED1.o: ../Generated_Code/LED1.c ../Generated_Code/LED1.h \
+ ../Generated_Code/PE_Types.h ../Generated_Code/PE_Error.h \
+ ../Generated_Code/PE_Const.h ../Generated_Code/IO_Map.h \
+ C:/Users/Sienna/Documents/Uni\ Work/CC2511-2017/NewWorkSpace/Sienna-Rose-Price-Labs/LabAssignmentDice/Static_Code/IO_Map/MK20D5.h \
+ ../Generated_Code/BitIoLdd3.h \
+ C:/Users/Sienna/Documents/Uni\ Work/CC2511-2017/NewWorkSpace/Sienna-Rose-Price-Labs/LabAssignmentDice/Static_Code/PDD/GPIO_PDD.h \
+ C:/Users/Sienna/Documents/Uni\ Work/CC2511-2017/NewWorkSpace/Sienna-Rose-Price-Labs/LabAssignmentDice/Static_Code/PDD/PDD_Types.h \
+ ../Generated_Code/Cpu.h
+
+../Generated_Code/LED1.h:
+
+../Generated_Code/PE_Types.h:
+
+../Generated_Code/PE_Error.h:
+
+../Generated_Code/PE_Const.h:
+
+../Generated_Code/IO_Map.h:
+
+C:/Users/Sienna/Documents/Uni\ Work/CC2511-2017/NewWorkSpace/Sienna-Rose-Price-Labs/LabAssignmentDice/Static_Code/IO_Map/MK20D5.h:
+
+../Generated_Code/BitIoLdd3.h:
+
+C:/Users/Sienna/Documents/Uni\ Work/CC2511-2017/NewWorkSpace/Sienna-Rose-Price-Labs/LabAssignmentDice/Static_Code/PDD/GPIO_PDD.h:
+
+C:/Users/Sienna/Documents/Uni\ Work/CC2511-2017/NewWorkSpace/Sienna-Rose-Price-Labs/LabAssignmentDice/Static_Code/PDD/PDD_Types.h:
+
+../Generated_Code/Cpu.h:
---
+++ LabAssignmentDice/LabAssignmentDice/Debug/Project_Settings/Startup_Code/startup.d
@@ -0,0 +1,8 @@
+Project_Settings/Startup_Code/startup.o: \
+ ../Project_Settings/Startup_Code/startup.c \
+ C:/Users/Sienna/Documents/Uni\ Work/CC2511-2017/NewWorkSpace/Sienna-Rose-Price-Labs/LabAssignmentDice/Generated_Code/IO_Map.h \
+ C:/Users/Sienna/Documents/Uni\ Work/CC2511-2017/NewWorkSpace/Sienna-Rose-Price-Labs/LabAssignmentDice/Static_Code/IO_Map/MK20D5.h
+
+C:/Users/Sienna/Documents/Uni\ Work/CC2511-2017/NewWorkSpace/Sienna-Rose-Price-Labs/LabAssignmentDice/Generated_Code/IO_Map.h:
+
+C:/Users/Sienna/Documents/Uni\ Work/CC2511-2017/NewWorkSpace/Sienna-Rose-Price-Labs/LabAssignmentDice/Static_Code/IO_Map/MK20D5.h:
